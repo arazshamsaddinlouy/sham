9f731ad66cdb013956a2b480db1fa66a2b92d932
arazshamsaddinlouy <arazshamsaddin@gmail.com>
2025-01-24 12:19:35 +0330

    fix

--- sham/src/components/home-order-flow.tsx
+++ sham/src/components/home-order-flow.tsx
@@ -6,141 +6,81 @@ export default function HomeOrderFlow() {
       <div className="pb-[30px]">
         <SectionHeadings title={"ثبت سفارش"} />
       </div>
-      <div className="container mx-auto">
-        <div className="flex mb-[60px] gap-[15px]">
-          <div className="flex-1 basis-1/3 mb-[40px]">
-            <div className="flex flex-wrap">
-              <div className="w-[100px]">
-                <div className="w-[50px] h-[50px] opacity-[0.4] text-[82px] relative">
-                  <div className="relative z-[2]">۱</div>
-                  <div className="w-[50px] h-[50px] opacity-[0.2] bg-blue-600 absolute rounded-[4px] right-[10px] top-[10px]" />
-                </div>
-              </div>
-              <div className="flex-1">
-                <div className="flex flex-col">
-                  <div className="text-[23px] mb-[10px]">ثبت درخواست کالا</div>
-                  <div className="text-[14px]">
-                    در این بخش شما با درخواست کالا مورد نظر خود به تمام فروشنده
-                    های اطراف خود درخواست خود را ارسال میکنید در این بخش شما با
-                    درخواست کالا مورد نظر خود به تمام فروشنده های اطراف خود
-                    درخواست خود را ارسال میکنید
+      <div className="">
+        <div className="h-[460px] pt-[30px]">
+          <div className="relative overflow-hidden">
+            <div className="container mx-auto">
+              <div className="flex h-[460px] items-center">
+                <div className="flex-1 relative">
+                  <div className="h-[3px] w-full bg-[#eee] reltive top-[15px]"></div>
+                  <div className="border-dashed border-[2px] border-[#eee] absolute right-[15px] bottom-[0px] h-[200px] w-[2px]" />
+                  <div className="border-dashed border-[2px] border-[#eee] absolute right-[20px] bottom-[200px] h-[2px] w-[40px]" />
+                  <div className="text-[#666] text-[18px] absolute right-[80px] bottom-[190px]">
+                    ثبت سفارش
+                  </div>
+                  <div className="absolute w-[36px] h-[36px] rounded-[35px] mt-[-20px] bg-[#fff] p-[6px] border-[3px] border-blue-400 overflow-hidden">
+                    <div className="w-[22px] h-[22px] rounded-[16px] bg-blue-400 relative top-[-2px] left-[2px]"></div>
                   </div>
                 </div>
-              </div>
-            </div>
-          </div>
-          <div className="flex-1 basis-1/3 mb-[40px]">
-            <div className="flex flex-wrap">
-              <div className="w-[100px]">
-                <div className="w-[50px] h-[50px] opacity-[0.4] text-[82px] relative">
-                  <div className="relative z-[2]">۲</div>
-                  <div className="w-[50px] h-[50px] opacity-[0.2] bg-blue-600 absolute rounded-[4px] right-[10px] top-[10px]" />
-                </div>
-              </div>
-              <div className="flex-1">
-                <div className="flex flex-col">
-                  <div className="text-[23px] mb-[10px]">ثبت درخواست کالا</div>
-                  <div className="text-[14px]">
-                    در این بخش شما با درخواست کالا مورد نظر خود به تمام فروشنده
-                    های اطراف خود درخواست خود را ارسال میکنید در این بخش شما با
-                    درخواست کالا مورد نظر خود به تمام فروشنده های اطراف خود
-                    درخواست خود را ارسال میکنید در این بخش شما با درخواست کالا
-                    مورد نظر خود به تمام فروشنده های اطراف خود درخواست خود را
-                    ارسال میکنید
+                <div className="flex-1 relative">
+                  <div className="h-[3px] w-full bg-[#eee] reltive top-[15px]"></div>
+                  <div className="border-dashed border-[2px] border-[#eee] absolute right-[15px] top-[0px] h-[200px] w-[2px]" />
+                  <div className="border-dashed border-[2px] border-[#eee] absolute right-[20px] top-[200px] h-[2px] w-[40px]" />
+                  <div className="text-[#666] text-[18px] absolute right-[80px] top-[190px]">
+                    تخمین قیمت
+                  </div>
+                  <div className="absolute w-[36px] h-[36px] rounded-[35px] mt-[-20px] bg-[#fff] p-[6px] border-[3px] border-blue-400 overflow-hidden">
+                    <div className="w-[22px] h-[22px] rounded-[16px] bg-blue-400 relative top-[-2px] left-[2px]"></div>
                   </div>
                 </div>
-              </div>
-            </div>
-          </div>
-          <div className="flex-1 basis-1/3 mb-[40px]">
-            <div className="flex flex-wrap">
-              <div className="w-[100px]">
-                <div className="w-[50px] h-[50px] opacity-[0.4] text-[82px] relative">
-                  <div className="relative z-[2]">۳</div>
-                  <div className="w-[50px] h-[50px] opacity-[0.2] bg-blue-600 absolute rounded-[4px] right-[10px] top-[10px]" />
-                </div>
-              </div>
-              <div className="flex-1">
-                <div className="flex flex-col">
-                  <div className="text-[23px] mb-[10px]">ثبت درخواست کالا</div>
-                  <div className="text-[14px]">
-                    در این بخش شما با درخواست کالا مورد نظر خود به تمام فروشنده
-                    های اطراف خود درخواست خود را ارسال میکنید در این بخش شما با
-                    درخواست کالا مورد نظر خود به تمام فروشنده های اطراف خود
-                    درخواست خود را ارسال میکنید در این بخش شما با درخواست کالا
-                    مورد نظر خود به تمام فروشنده های اطراف خود درخواست خود را
-                    ارسال میکنید
+                <div className="flex-1 relative">
+                  <div className="h-[3px] w-full bg-[#eee] reltive top-[15px]"></div>
+                  <div className="border-dashed border-[2px] border-[#eee] absolute right-[15px] bottom-[0px] h-[200px] w-[2px]" />
+                  <div className="border-dashed border-[2px] border-[#eee] absolute right-[20px] bottom-[200px] h-[2px] w-[40px]" />
+                  <div className="text-[#666] text-[18px] absolute right-[80px] bottom-[190px]">
+                    یافتن ارزان ترین قیمت
+                  </div>
+                  <div className="absolute w-[36px] h-[36px] rounded-[35px] mt-[-20px] bg-[#fff] p-[6px] border-[3px] border-blue-400 overflow-hidden">
+                    <div className="w-[22px] h-[22px] rounded-[16px] bg-blue-400 relative top-[-2px] left-[2px]"></div>
                   </div>
                 </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="flex mb-[60px] gap-[15px]">
-          <div className="flex-1 basis-1/3 mb-[40px]">
-            <div className="flex flex-wrap">
-              <div className="w-[100px]">
-                <div className="w-[50px] h-[50px] opacity-[0.4] text-[82px] relative">
-                  <div className="relative z-[2]">۴</div>
-                  <div className="w-[50px] h-[50px] opacity-[0.2] bg-blue-600 absolute rounded-[4px] right-[10px] top-[10px]" />
-                </div>
-              </div>
-              <div className="flex-1">
-                <div className="flex flex-col">
-                  <div className="text-[23px] mb-[10px]">ثبت درخواست کالا</div>
-                  <div className="text-[14px]">
-                    در این بخش شما با درخواست کالا مورد نظر خود به تمام فروشنده
-                    های اطراف خود درخواست خود را ارسال میکنید در این بخش شما با
-                    درخواست کالا مورد نظر خود به تمام فروشنده های اطراف خود
-                    درخواست خود را ارسال میکنید در این بخش شما با درخواست کالا
-                    مورد نظر خود به تمام فروشنده های اطراف خود درخواست خود را
-                    ارسال میکنید
+                <div className="flex-1 relative">
+                  <div className="h-[3px] w-full bg-[#eee] reltive top-[15px]"></div>
+                  <div className="border-dashed border-[2px] border-[#eee] absolute right-[15px] top-[0px] h-[200px] w-[2px]" />
+                  <div className="border-dashed border-[2px] border-[#eee] absolute right-[20px] top-[200px] h-[2px] w-[40px]" />
+                  <div className="text-[#666] text-[18px] absolute right-[80px] top-[190px]">
+                    تخمین قیمت
+                  </div>
+                  <div className="absolute w-[36px] h-[36px] rounded-[35px] mt-[-20px] bg-[#fff] p-[6px] border-[3px] border-blue-400 overflow-hidden">
+                    <div className="w-[22px] h-[22px] rounded-[16px] bg-blue-400 relative top-[-2px] left-[2px]"></div>
                   </div>
                 </div>
-              </div>
-            </div>
-          </div>
-          <div className="flex-1 basis-1/3 mb-[40px]">
-            <div className="flex flex-wrap">
-              <div className="w-[100px]">
-                <div className="w-[50px] h-[50px] opacity-[0.4] text-[82px] relative">
-                  <div className="relative z-[2]">۵</div>
-                  <div className="w-[50px] h-[50px] opacity-[0.2] bg-blue-600 absolute rounded-[4px] right-[10px] top-[10px]" />
-                </div>
-              </div>
-              <div className="flex-1">
-                <div className="flex flex-col">
-                  <div className="text-[23px] mb-[10px]">ثبت درخواست کالا</div>
-                  <div className="text-[14px]">
-                    در این بخش شما با درخواست کالا مورد نظر خود به تمام فروشنده
-                    های اطراف خود درخواست خود را ارسال میکنید در این بخش شما با
-                    درخواست کالا مورد نظر خود به تمام فروشنده های اطراف خود
-                    درخواست خود را ارسال میکنید در این بخش شما با درخواست کالا
-                    مورد نظر خود به تمام فروشنده های اطراف خود درخواست خود را
-                    ارسال میکنید
+                <div className="flex-1 relative">
+                  <div className="h-[3px] w-full bg-[#eee] reltive top-[15px]"></div>
+                  <div className="border-dashed border-[2px] border-[#eee] absolute right-[15px] bottom-[0px] h-[200px] w-[2px]" />
+                  <div className="border-dashed border-[2px] border-[#eee] absolute right-[20px] bottom-[200px] h-[2px] w-[40px]" />
+                  <div className="text-[#666] text-[18px] absolute right-[80px] bottom-[190px]">
+                    انتخاب فروشنده
+                  </div>
+                  <div className="absolute w-[36px] h-[36px] rounded-[35px] mt-[-20px] bg-[#fff] p-[6px] border-[3px] border-blue-400 overflow-hidden">
+                    <div className="w-[22px] h-[22px] rounded-[16px] bg-blue-400 relative top-[-2px] left-[2px]"></div>
                   </div>
                 </div>
-              </div>
-            </div>
-          </div>
-          <div className="flex-1 basis-1/3 mb-[40px]">
-            <div className="flex flex-wrap">
-              <div className="w-[100px]">
-                <div className="w-[50px] h-[50px] opacity-[0.4] text-[82px] relative">
-                  <div className="relative z-[2]">۶</div>
-                  <div className="w-[50px] h-[50px] opacity-[0.2] bg-blue-600 absolute rounded-[4px] right-[10px] top-[10px]" />
-                </div>
-              </div>
-              <div className="flex-1">
-                <div className="flex flex-col">
-                  <div className="text-[23px] mb-[10px]">ثبت درخواست کالا</div>
-                  <div className="text-[14px]">
-                    در این بخش شما با درخواست کالا مورد نظر خود به تمام فروشنده
-                    های اطراف خود درخواست خود را ارسال میکنید در این بخش شما با
-                    درخواست کالا مورد نظر خود به تمام فروشنده های اطراف خود
-                    درخواست خود را ارسال میکنید در این بخش شما با درخواست کالا
-                    مورد نظر خود به تمام فروشنده های اطراف خود درخواست خود را
-                    ارسال میکنید
+                <div className="flex-1 relative">
+                  <div className="h-[3px] w-full bg-[#eee] reltive top-[15px]"></div>
+                  <div className="border-dashed border-[2px] border-[#eee] absolute right-[15px] top-[0px] h-[200px] w-[2px]" />
+                  <div className="border-dashed border-[2px] border-[#eee] absolute right-[20px] top-[200px] h-[2px] w-[40px]" />
+                  <div className="text-[#666] text-[18px] absolute right-[80px] top-[190px]">
+                    پرداخت و دریافت کالا
+                  </div>
+                  <div className="absolute w-[36px] h-[36px] rounded-[35px] mt-[-20px] bg-[#fff] p-[6px] border-[3px] border-blue-400 overflow-hidden">
+                    <div className="w-[22px] h-[22px] rounded-[16px] bg-blue-400 relative top-[-2px] left-[2px]"></div>
+                  </div>
+                  <div className="text-[#666] text-[18px] absolute right-[170px] w-[105px] top-[30px]">
+                    تکمیل سفارش
+                  </div>
+                  <div className="absolute w-[36px] h-[36px] left-[0px] rounded-[35px] mt-[-20px] bg-[#fff] p-[6px] border-[3px] border-blue-400 overflow-hidden">
+                    <div className="w-[22px] h-[22px] rounded-[16px] bg-blue-400 relative top-[-2px] left-[2px]"></div>
                   </div>
                 </div>
               </div>
